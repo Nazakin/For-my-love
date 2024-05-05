4f8e452deba51f2f2ac5def26e03c353dca3b1ed
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-05-06 01:14:26 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import css from './App.module.css'
+
 export const App = () => {
+  const [answer, setAnswer] = useState(null)
+  const [sadImage, setSadImage] = useState(false)
+  const handleAnswer = (e) => {
+    setAnswer(e.target.value)
+  }
+  const handleSubmit = () => {
+    if (answer === 'ні' || answer === 'Ні') {
+      alert("Чому?")
+    } else if (answer === 'так' || answer === 'Так'){
+      alert ('І я тебе кохаю, зай)))))')
+    } else if (!answer) {
+      alert("Може заповниш?)")
+    } else {
+      alert("У тебе два запропоновані варінати!!! НІ ГРАМОМ БІЛЬШЕ!!")
+    }
+  }
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h2>Любиш мене? Так чи ні?</h2>
+      <input placeholder='Пиши сюди блін' value={answer} onChange={handleAnswer} className={css.input} />
+      <button type='button' onClick={handleSubmit} className={css.button}>ТИЦЬ</button>
     </div>
   );
 };
